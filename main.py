a237f91958261f09399a64f5f5261a30415598a4
Nandi Nkhowani <nandinkhowani@Nandis-MBP.lan>
2023-12-11 20:03:33 +0100

    Third commit Added comments to the code

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class DataValidator:
 
         return self.valid_integers
 
+    # Check if value is positive
     def _is_valid_positive_integer(self, input_str):
         try:
             value = int(input_str)
@@ -22,5 +23,6 @@ user_inputs = ["12", "abc", "45", "-3", "100"]
 validator = DataValidator()
 valid_integers = validator.validate_and_add_integers(user_inputs)
 
+# Print example test output
 print("Original User Inputs:", user_inputs)
 print("Valid Positive Integers:", valid_integers)
